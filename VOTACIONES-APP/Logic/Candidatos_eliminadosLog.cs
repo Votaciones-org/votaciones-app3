2d3ae623d4d3cc48364682b03096c6a03796558e
92Gomez <mr.diego92gomez@gmail.com>
2024-10-28 16:44:26 -0500

    Se crea funcionalidades capa Logic

---
+++ VOTACIONES-APP/Logic/Candidatos_eliminadosLog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Logic
+{
+    public class Candidatos_eliminadosLog
+    {
+        Persistence objCanEl = new Persistence();
+
+        public DataSet showCandidatosEliminados()
+        {
+
+            return objCanEl.showCandidatosEliminados();
+        }
+
+        public bool saveCandidatoEliminado(string nombre, string apellido, string partido, string fechaNacimiento, string propuesta)
+        {
+
+            return objCanEl.saveCandidatoEliminado(nombre, apellido, partido, fechaNacimiento, propuesta);
+        }
+
+        public bool updateCandidatoEliminado(int id, string nombre, string apellido, string partido, string fechaNacimiento, string propuesta)
+        {
+
+            return objCanEl.updateCandidatoEliminado(id, nombre, apellido, partido, fechaNacimiento, propuesta);
+        }
+
+        public bool deleteCandidatoEliminado(int id)
+        {
+
+            return objCanEl.deleteCandidatoEliminado(id);
+        }
+    }
+}
