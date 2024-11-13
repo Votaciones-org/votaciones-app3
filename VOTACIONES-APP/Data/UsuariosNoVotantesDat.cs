84f71d90ec7ab2e5dbfbb8750d4b64b1d52a4661
Juan esteban molano <juanestebanmolanog@gmail.com>
2024-11-12 21:03:22 -0500

    actualizacion 2.2

---
+++ VOTACIONES-APP/Data/UsuariosNoVotantesDat.cs
@@ -0,0 +1,124 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+
+namespace Data
+{
+    public class UsuariosNoVotantesDat
+    {
+        // Se crea una instancia de la clase Persistence para manejar la conexión a la base de datos.
+        Persistence objPer = new Persistence();
+
+        // Método para mostrar los usuarios no votantes desde la base de datos.
+        public DataSet showUsuariosNoVotantes()
+        {
+            MySqlDataAdapter objAdapter = new MySqlDataAdapter();
+            DataSet objData = new DataSet();
+            MySqlCommand objSelectCmd = new MySqlCommand();
+
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "procSelectUsuariosNoVotantes"; // Procedimiento almacenado para seleccionar los usuarios
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            objAdapter.SelectCommand = objSelectCmd;
+            objAdapter.Fill(objData);
+
+            objPer.closeConnection();
+
+            return objData;
+        }
+
+        // Método para guardar un nuevo usuario no votante
+        public bool saveUsuarioNoVotante(string _nombre, string _apellido, string _cedula, string _opcion)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "procInsertUsuarioNoVotante"; // Procedimiento almacenado para insertar un nuevo usuario
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            objSelectCmd.Parameters.Add("p_nombre", MySqlDbType.VarString).Value = _nombre;
+            objSelectCmd.Parameters.Add("p_apellido", MySqlDbType.VarString).Value = _apellido;
+            objSelectCmd.Parameters.Add("p_cedula", MySqlDbType.VarString).Value = _cedula;
+            objSelectCmd.Parameters.Add("p_opcion", MySqlDbType.VarString).Value = _opcion;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+
+        // Método para actualizar un usuario no votante
+        public bool updateUsuarioNoVotante(int _id, string _nombre, string _apellido, string _cedula, string _opcion)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "procUpdateUsuarioNoVotante"; // Procedimiento almacenado para actualizar un usuario
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            objSelectCmd.Parameters.Add("p_id", MySqlDbType.Int32).Value = _id;
+            objSelectCmd.Parameters.Add("p_nombre", MySqlDbType.VarString).Value = _nombre;
+            objSelectCmd.Parameters.Add("p_apellido", MySqlDbType.VarString).Value = _apellido;
+            objSelectCmd.Parameters.Add("p_cedula", MySqlDbType.VarString).Value = _cedula;
+            objSelectCmd.Parameters.Add("p_opcion", MySqlDbType.VarString).Value = _opcion;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+
+        // Método para eliminar un usuario no votante
+        public bool deleteUsuarioNoVotante(int _idUsuario)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "procDeleteVotoUsuarioNoVotanteDDL"; // Procedimiento almacenado para eliminar un usuario
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("p_id", MySqlDbType.Int32).Value = _idUsuario;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+    }
+}
---
+++ VOTACIONES-APP/Presentacion/AWUsuariosNoVotantes.aspx.designer.cs
@@ -0,0 +1,160 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+<<<<<<< HEAD
+<<<<<<< HEAD
+namespace Presentation
+{
+
+
+    public partial class AWFUsuariosNoVotantes
+    {
+
+        /// <summary>
+        /// Control HFIdUsuario.
+=======
+=======
+>>>>>>> 288737810150671c46f9d199d2c7e3f73c1e7378
+namespace Presentacion
+{
+
+
+    public partial class AWUsuariosNoVotantes
+    {
+
+        /// <summary>
+        /// Control GVProducts.
+<<<<<<< HEAD
+>>>>>>> 288737810150671c46f9d199d2c7e3f73c1e7378
+=======
+>>>>>>> 288737810150671c46f9d199d2c7e3f73c1e7378
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+<<<<<<< HEAD
+<<<<<<< HEAD
+        protected global::System.Web.UI.WebControls.HiddenField HFIdUsuario;
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Control no_nombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox no_nombre;
+
+        /// <summary>
+        /// Control Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Control no_apellido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox no_apellido;
+
+        /// <summary>
+        /// Control Label3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+
+        /// <summary>
+        /// Control no_cedula.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox no_cedula;
+
+        /// <summary>
+        /// Control Label4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+
+        /// <summary>
+        /// Control no_opcion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox no_opcion;
+
+        /// <summary>
+        /// Control btnGuardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGuardar;
+
+        /// <summary>
+        /// Control btnActualizar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnActualizar;
+
+        /// <summary>
+        /// Control LblMsj.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblMsj;
+
+        /// <summary>
+        /// Control GVUsuariosNoVotantes.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GVUsuariosNoVotantes;
+=======
+        protected global::System.Web.UI.WebControls.GridView GVProducts;
+>>>>>>> 288737810150671c46f9d199d2c7e3f73c1e7378
+=======
+        protected global::System.Web.UI.WebControls.GridView GVProducts;
+>>>>>>> 288737810150671c46f9d199d2c7e3f73c1e7378
+    }
+}
